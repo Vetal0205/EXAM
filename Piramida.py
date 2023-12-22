1926604030fb0b4c1c0b7a1f4c3f5d0f7d043718
Vitalii Besliubniak <vetal0205merkulov@gmail.com>
2023-12-22 04:04:24 -0600

    Upd

--- Piramida.py
+++ Piramida.py
@@ -4,5 +4,5 @@ def pyramid_number(n):
     return n * (n + 1) * (n + 2) // 6
 
 n = 5
-print("Беслюбняк Віталій Сергійович МІТ-31")
+print("Беслюбняк Віталій Сергійович Студент МІТ-31")
 print(f"Pyramid number for n={n} is {pyramid_number(n)}")
